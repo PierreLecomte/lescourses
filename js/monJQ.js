01e07579c23b4ea8a8915a3de1a9165ef9c31536
Pierre Lecomte <pierre.lecomte.86@gmail.com>
2017-11-24 17:02:06 +0100

    AJAX - Cases à cocher réversibles ! Déclenchement au clic sur une classe fixe Le déclenchement sur des classes amovibles ne fonctionne plus après changement de la classe.

--- js/monJQ.js
+++ js/monJQ.js
@@ -11,50 +11,47 @@ jQuery(function($){
 	$('#footer').load('ajax/footer.html');
 
 
-	$('.fa-trash-o').click(function(){
+	$('.fa').on('click',function(){
+		var btn_class = $(this).attr('class');
 		var id_clic = $(this).attr('data-id_produit');
-		$.ajax({
-			url : 'ajax/supprimer.php',
+		console.log(id_clic);
+
+		if (btn_class == "fa fa-square-o"){
+			$.ajax({
+				url : 'ajax/cocher.php',
+				data : {id : id_clic},
+				dataType : 'script',
+				success: function(){
+					$('#id_' + id_clic + ' .fa-square-o').removeClass('fa-square-o').addClass('fa-check');
+				}
+			});
+		}
+
+
+		else if (btn_class == "fa fa-check"){
+
+			$.ajax({
+				url : 'ajax/decocher.php',
+				data : {id : id_clic},
+				dataType : 'script',
+				success: function(){
+					$('#id_' + id_clic + ' .fa-check').removeClass('fa-check').addClass('fa-square-o');
+				}
+			});
+		}
+
+		else if (btn_class == "fa fa-trash-o"){
+			$.ajax({
+				url : 'ajax/supprimer.php',
 			//data : permet d'envoyer sous forme d'objet des données en GET à la page php
 			data : {id : id_clic},
 			dataType : 'script'
 			/*success: function(){
 				jQuery('#id_' + id_clic).remove();*/
 			});
+		}
 
 	});
-
-
-	$('.fa-square-o').on('click',function(){
-		var id_clic = $(this).attr('data-id_produit');
-		console.log(id_clic);
-		$.ajax({
-			url : 'ajax/cocher.php',
-			data : {id : id_clic},
-			dataType : 'script',
-			success: function(){
-				$('#id_' + id_clic + ' .fa-square-o').removeClass('fa-square-o').addClass('fa-check');
-				}
-			});
-
-	});
-
-
-	$('.fa-check').on('click',function(){
-		var id_clic = $(this).attr('data-id_produit');
-		console.log(id_clic);
-		$.ajax({
-			url : 'ajax/decocher.php',
-			data : {id : id_clic},
-			dataType : 'script',
-			success: function(){
-				$('#id_' + id_clic + ' .fa-check').removeClass('fa-check').addClass('fa-square-o');
-				}
-			});
-
-	});
-
-
 })
 
 
